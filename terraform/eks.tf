e9c5104ed454dfd67d69738e7fcb9f9a447619be
shelby-moore <shelbylmoore@gmail.com>
2023-04-22 18:21:40 -0700

    allow all node to node ingress

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -58,6 +58,17 @@ module "eks" {
     }
   ]
 
+  node_security_group_additional_rules = {
+    ingress_self_all = {
+      description = "Node to node all ports/protocols"
+      protocol    = "-1"
+      from_port   = 0
+      to_port     = 0
+      type        = "ingress"
+      self        = true
+    }
+  }
+
   node_security_group_tags = {
     "karpenter.sh/discovery" = local.name
   }
